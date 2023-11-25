83d16ab92e1209ea199956afeb56eeb2d27d0088
Laplace Ricky <wailun089@gmail.com>
2023-11-26 00:31:10 +0800

    add lsall mode more outputs

--- git_system.py
+++ git_system.py
@@ -3,12 +3,18 @@ import argparse,sys
 import subprocess
 from lib_io import limited_lines
 
+def ls_all():
+    r = subprocess.run(['git','log','--all','--pretty=format:','--name-status'], stdout = subprocess.PIPE, text = True, check = True).stdout.splitlines()
+    r = [ x for x in r if x != '' and x.split()[0] != 'M']
+    deleted_files = [ x.split()[1] for x in r if x.split()[0] == 'D']
+    return [ x for x in r if not (x.split()[0] == 'A' and x.split()[1] in deleted_files)]
+
 def ls_tree():
-    return subprocess.run(['git', 'ls-tree', '--full-tree', '--name-only', '-r', 'HEAD'], stdout = subprocess.PIPE, text = True, check = True).stdout.splitlines()
+    return subprocess.run(['git', 'ls-tree', '--full-tree', '-r', '--format=%(objectmode) %(objecttype) %x09%(path)', 'HEAD'], stdout = subprocess.PIPE, text = True, check = True).stdout.splitlines()
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('mode', choices = ['ls'] )
+    parser.add_argument('mode', choices = ['ls','lsall'] )
     parser.add_argument('target', nargs='*')
     parser.add_argument('-n', '--head', type=int, default = 10)
     parser.add_argument('-a','--all',action='store_true')
@@ -17,21 +23,20 @@ def main():
     match args.mode:
         case 'ls':
             assert not args.target
+            worker_func = ls_tree
+        case 'lsall':
+            assert not args.target
+            worker_func = ls_all
 
-    match args.mode:
-        case 'ls':
-            result = ls_tree()
-
-    match args.mode:
-        case 'ls':
-            start = 0
-            end = args.head
-            step = 1
+    result = worker_func()
 
     if args.all:
         for x in result:
             print(x)
     else:
+        start = 0
+        end = args.head
+        step = 1
         print(limited_lines(result, start, end, step, more_info = True))
 
 
